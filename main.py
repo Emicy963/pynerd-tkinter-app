595232e584ce60c7702767e8db778e83eb969542
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-05-04 02:13:12 +0100

    feat(label): Create and add a label on window

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ windows.title("PyNerd App")
 windows.geometry("700x400")
 windows.resizable(False, False)
 
+Label(windows, text="E aí, PyNerd!", bg="black", fg="white", padx=30, pady=30).grid(row=0, column=0)
+
 windows.mainloop()
