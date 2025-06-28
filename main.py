b80e88bc787f8c6a4360b7119a203f7438ce67eb
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-06-28 02:30:52 +0100

    feat(buttons): Created and add the button on windows

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from tkinter import *
 
+def mostrar():
+    armazem_label["text"] = "Button Click"
+
 windows = Tk()
 windows.title("PyNerd App")
 windows.geometry("700x400")
 windows.resizable(False, False)
 
-Entry(windows, bg="black", fg="orange", show="*").grid(row=0, column=0)
+Button(windows, text="Click Here", bg="black", fg="white", height=10, width=20, command=mostrar).grid(row=0, column=0)
+
+armazem_label = Label(windows, text="No Click")
+armazem_label.grid(row=1, column=0)
 
 windows.mainloop()
